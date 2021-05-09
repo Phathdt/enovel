876ee4f2dd43f620262d61ae1d545ad17b9d2fa6
Phathdt <phathdt379@gmail.com>
2021-05-09 09:27:33 +0700

    using devise

--- enovel-api/app/cores/account/user.rb
+++ enovel-api/app/cores/account/user.rb
@@ -3,5 +3,7 @@
 module Account
   class User < ApplicationRecord
     self.table_name = 'users'
+
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   end
 end
--- enovel-api/db/seeds.rb
+++ enovel-api/db/seeds.rb
@@ -1,7 +1,8 @@
-100.times do
+100.times do |t|
+  puts ">>>>>>>>>>>>>>>>>"
+  puts "time is #{t}"
   Account::User.create(
     email: Faker::Internet.email,
     password: Faker::Number.number(digits: 10),
-    age: Faker::Number.number(digits: 2),
   )
 end
